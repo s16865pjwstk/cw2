fa04aac0cbd60415c11e0567b33e408edc94b86a
unknown <Adam Jatkowski>
2020-03-22 19:03:17 +0100

    Zadanie 3

---
+++ Cw2/Cw2/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw2.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+
+    }
+}
